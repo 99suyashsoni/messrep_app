3d84d75412a698a590aaa5542bc25e05ef8198b1
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2020-02-07 17:43:05 +0530

    Completed database helper

--- lib/util/database_helper.dart
+++ lib/util/database_helper.dart
@@ -11,7 +11,17 @@ Future<Database> databaseInstance(String dbName) async {
   String path = join(await getDatabasesPath(), dbName);
   _database = await openDatabase(path, version: 1, onCreate: (db, _) async {
     await db.transaction((txn) async {
+      await txn.execute('''PRAGMA foreign_keys = ON''');
 
+      await txn.execute('''
+        CREATE TABLE Issues(
+          id  INTEGER PRIMARY KEY,
+          title TEXT NOT NULL,
+          upvotedCount INTEGER NOT NULL,
+        )
+      ''');
     });
   });
+
+  return _database;
 }
